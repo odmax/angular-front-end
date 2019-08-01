34f1888067f9e9c32dec6c2b0d597ce458ce4277
ICEP-MOBILE-004 <thatobenny2@gmail.com>
2019-08-01 14:39:58 +0200

    check my work-palesa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Palesa2Component } from './palesa2/palesa2.component';
+import { Palesa2 } from './palesa2.html/palesa2.html.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Palesa2Component,
+    Palesa2.HtmlComponent
   ],
   imports: [
     BrowserModule,
