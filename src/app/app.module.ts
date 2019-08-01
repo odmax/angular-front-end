5f8b6969dead486c7d87bd06a1d33906c7223751
Odmax <Ademoyemo@gmail.com>
2019-08-01 14:35:17 +0200

    first  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Moses } from './moses.html/moses.html.component';
+import { MosesComponent } from './moses/moses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Moses.HtmlComponent,
+    MosesComponent
   ],
   imports: [
     BrowserModule,
