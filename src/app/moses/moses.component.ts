3171efe78d30bdf5519351650d2af1c1d5300b57
MosesMulaudzi <mosesdakalo17@gmail.com>
2019-08-01 14:50:01 +0200

    My work

---
+++ src/app/moses/moses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moses',
+  templateUrl: './moses.component.html',
+  styleUrls: ['./moses.component.scss']
+})
+export class MosesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
